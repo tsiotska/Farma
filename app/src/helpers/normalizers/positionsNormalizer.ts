1681f2b4087846fad9c57d55c0d4fe12c5e06978
User <m.ossas@cleverdec.com>
2020-03-31 11:05:59 +0300

    add permisions to positions

--- app/src/helpers/normalizers/positionsNormalizer.ts
+++ app/src/helpers/normalizers/positionsNormalizer.ts
@@ -1,16 +1,19 @@
 import { IValuesMap, objectArrayNormalizer } from './normalizer';
 import { IPosition } from '../../interfaces/IPosition';
+import { PERMISSIONS } from '../../constants/Permissions';
 
 const defaultPosition: IPosition = {
     id: null,
     name: null,
-    alias: null
+    alias: null,
+    permissions: []
 };
 
 const valuesMap: IValuesMap = {
     id: 'id',
     name: 'name',
     alias: 'alias',
+    permissions: 'permissions'
 };
 
 export const positionsNormalizer = ({ data: { data }}: any) => objectArrayNormalizer(
@@ -18,6 +21,14 @@ export const positionsNormalizer = ({ data: { data }}: any) => objectArrayNormal
     defaultPosition,
     valuesMap,
     {
-        requiredProps: [ 'id', 'name' ]
+        requiredProps: [ 'id', 'name' ],
+        valueNormalizers: {
+            permissions: (value: string[]) =>
+                value.map(x => (
+                    !!PERMISSIONS[x]
+                    ? x
+                    : undefined
+                )).filter(x => !!x)
+        }
     }
 );
--- app/src/interfaces/IPosition.ts
+++ app/src/interfaces/IPosition.ts
@@ -1,5 +1,8 @@
+import { PERMISSIONS } from '../constants/Permissions';
+
 export interface IPosition {
     id: number;
     name: string;
     alias: string;
+    permissions: PERMISSIONS[];
 }
