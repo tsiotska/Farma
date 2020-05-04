e416de16cc94cf1d7c03200224f20bc61d00f0ce
Maxim Ossas <m.ossas@cleverdec.com>
2020-05-04 11:10:53 +0300

    align HCF list items at the top edge

--- app/src/containers/HCFList/ListItem/ListItem.tsx
+++ app/src/containers/HCFList/ListItem/ListItem.tsx
@@ -123,7 +123,7 @@ class ListItem extends Component<IProps> {
         } = this.props;
 
         return (
-            <Grid className={classes.root} alignItems='center' container>
+            <Grid className={classes.root} alignItems='flex-start' container>
                 <Grid className={cx(classes.cell, classes.name)} xs alignItems='center' container item>
                     {
                         unconfirmed &&
