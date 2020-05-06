4f3fbb8c558569b4a4e23db74dd583d8a319cede
Maxim Ossas <m.ossas@cleverdec.com>
2020-05-06 21:18:59 +0300

    initial DoctorModal

--- app/src/containers/Doctors/DoctorModal/DoctorModal.tsx
+++ app/src/containers/Doctors/DoctorModal/DoctorModal.tsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { createStyles, WithStyles } from '@material-ui/core';
 import { observer } from 'mobx-react';
 import { withStyles } from '@material-ui/styles';
+import { ISpecialty } from '../../../interfaces/ISpecialty';
+import { ILPU } from '../../../interfaces/ILPU';
+import Dialog from '../../../components/Dialog';
+import FormRow from '../../../components/FormRow';
+import { observable } from 'mobx';
 
 const styles = (theme: any) => createStyles({
     submitButton: {
@@ -16,24 +21,71 @@ const styles = (theme: any) => createStyles({
 });
 
 interface IProps extends WithStyles<typeof styles> {
-
+    open: boolean;
+    isLoading: boolean;
+    onClose: () => void;
+    onSubmit: (values: IDoctorModalValues) => void;
+    lpus: ILPU[];
+    title: string;
 }
 
-// export interface IDoctorModalValues {
-//     name: string;
-//     lpu: string;
-//     specialty: IL;
-//     phone: string;
-//     card: string;
-// }
+export interface IDoctorModalValues {
+    [key: string]: string | ILPU | ISpecialty;
+    name: string;
+    lpu: ILPU;
+    specialty: ISpecialty;
+    homePhone: string;
+    workPhone: string;
+    card: string;
+}
 
+// @inject(() => ({
+//     specialties
+// }))
 @observer
 class DoctorModal extends Component<IProps> {
+    readonly objectFields: Array<keyof IDoctorModalValues> = [ 'lpu', 'specialty' ];
+    readonly initialFormValues: IDoctorModalValues = {
+        name: '',
+        lpu: null,
+        specialty: null,
+        homePhone: '',
+        workPhone: '',
+        card: '',
+    };
+    @observable formValues: IDoctorModalValues = { ...this.initialFormValues };
+
+    changeHandler = (propName: keyof IDoctorModalValues, value: string) => {
+        if (this.objectFields.includes(propName)) {
+            this.formValues[propName] = value;
+        } else {
+            this.formValues[propName] = value;
+        }
+    }
+
     render() {
+        const {
+            classes,
+            open,
+            onClose,
+            title
+        } = this.props;
         return (
-            <div>
-                DoctorModal
-            </div>
+            <Dialog
+            classes={{ title: classes.header }}
+            open={open}
+            onClose={onClose}
+            title={title}
+            fullWidth
+            maxWidth='sm'>
+                <FormRow
+                    label='ПІБ'
+                    values={this.formValues}
+                    onChange={this.changeHandler}
+                    propName='name'
+                    required
+                />
+            </Dialog>
         );
     }
 }
