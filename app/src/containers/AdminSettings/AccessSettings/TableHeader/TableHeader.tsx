657c9c66f9f8f1635716e279c01907c29fe4f737
Maxim Ossas <m.ossas@cleverdec.com>
2020-05-13 06:38:05 +0300

    refactored table code

--- app/src/containers/AdminSettings/AccessSettings/TableHeader/TableHeader.tsx
+++ app/src/containers/AdminSettings/AccessSettings/TableHeader/TableHeader.tsx
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import { createStyles, withStyles, Grid, WithStyles, Typography, IconButton } from '@material-ui/core';
-import { observer } from 'mobx-react';
+import React, {Component} from 'react';
+import {
+    createStyles,
+    withStyles,
+    Grid,
+    WithStyles,
+    Typography,
+    IconButton
+} from '@material-ui/core';
+import {observer} from 'mobx-react';
 import cx from 'classnames';
-import Settings from '-!react-svg-loader!../../../../../assets/icons/settings.svg';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 
 const styles = createStyles({
     fullWidth: {
@@ -21,20 +25,6 @@ const styles = createStyles({
     title_bold: {
         fontWeight: 600,
         color: '#808080'
-    },
-    complexHeader_icon: {
-        marginLeft: 10
-    },
-    headersWrapper: {
-        marginLeft: 10,
-        '& > p': {
-            display: 'flex',
-            alignItems: 'center',
-            height: 38, // to match with checkbox
-            textAlign: 'start',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis'
-        }
     }
 });
 
@@ -44,90 +34,156 @@ interface IProps extends WithStyles<typeof styles> {
 @observer
 class TableHeader extends Component<IProps> {
     render() {
-        const { classes } = this.props;
+        const {classes} = this.props;
 
         return (
             <Grid direction='column' wrap='nowrap' container>
 
-                <Grid className={cx(classes.complexHeader)} >
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Typography className={classes.title_bold} color='textSecondary' variant='body1'>
-                        Перегляд
-                    </Typography>
+                <Grid className={cx(classes.complexHeader)}>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography className={classes.title_bold}
+                                    color='textSecondary' variant='body1'>
+                            Перегляд
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography className={classes.title_bold} variant='body1'>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography className={classes.title_bold}
+                                    variant='body1'>
                             Звіт
                         </Typography>
                     </Grid>
                 </Grid>
 
-                <Grid className={cx(classes.complexHeader)} >
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography className={classes.title_bold} variant='body1'>
+                <Grid className={cx(classes.complexHeader)}>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography className={classes.title_bold}
+                                    variant='body1'>
                             Редагування
                         </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>Працівники</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            Працівники
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>ЗП</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            ЗП
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>Препарати</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            Препарати
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>ЛПУ</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            ЛПУ
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>Аптеки</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            Аптеки
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>Лікарі</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            Лікарі
+                        </Typography>
                     </Grid>
                 </Grid>
 
-                <Grid className={cx(classes.complexHeader)} >
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography className={classes.title_bold} variant='body1'>
+                <Grid className={cx(classes.complexHeader)}>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography className={classes.title_bold}
+                                    variant='body1'>
                             Додавання
                         </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>Препарати</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            Препарати
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>ЛПУ</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            ЛПУ
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>Аптеки</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            Аптеки
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>Лікарі</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            Лікарі
+                        </Typography>
                     </Grid>
                 </Grid>
 
-                <Grid className={cx(classes.complexHeader)} >
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography className={classes.title_bold} variant='body1'>
+                <Grid className={cx(classes.complexHeader)}>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography className={classes.title_bold}
+                                    variant='body1'>
                             Видалення
                         </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>Препарати</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            Препарати
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>ЛПУ</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            ЛПУ
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>Аптеки</Typography>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            Аптеки
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                        <Typography color='textSecondary' variant='body1'>Лікарі</Typography>
-                    </Grid>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary'
+                                    variant='body1'>
+                            Лікарі
+                        </Typography>
                     </Grid>
                 </Grid>
+            </Grid>
         );
     }
 }
--- app/src/containers/AdminSettings/AccessSettings/TableRow/TableRow.tsx
+++ app/src/containers/AdminSettings/AccessSettings/TableRow/TableRow.tsx
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
-import { createStyles, WithStyles, withStyles, Grid, Typography, Checkbox } from '@material-ui/core';
-import { observer } from 'mobx-react';
+import React, {Component} from 'react';
+import {
+    createStyles,
+    WithStyles,
+    withStyles,
+    Grid,
+    Typography,
+    Checkbox
+} from '@material-ui/core';
+import {observer} from 'mobx-react';
 import cx from 'classnames';
-import { PERMISSIONS } from '../../../../constants/Permissions';
+import {PERMISSIONS} from '../../../../constants/Permissions';
 
 const styles = createStyles({
     complexHeader: {
@@ -29,87 +36,143 @@ class TableRow extends Component<IProps> {
     changeHandler = (value: PERMISSIONS) => (e: any) => this.props.onChange(e, value);
 
     render() {
-        const { classes, title, permissions } = this.props;
+        const {classes, title, permissions} = this.props;
 
         return (
             <Grid direction='column' wrap='nowrap' container>
 
-                <Grid className={cx(classes.complexHeader)} >
-                <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Typography className={classes.title_bold} variant='body1'>
-                        {title}
-                    </Typography>
-                </Grid>
-                <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Typography variant='body1'>
-                        <Checkbox onChange={this.changeHandler(PERMISSIONS.SEE_REPORT)} checked={permissions.includes(PERMISSIONS.SEE_REPORT)} size='small' color='default' />
-                    </Typography>
-                </Grid>
-                </Grid>
-
-                <Grid className={cx(classes.complexHeader)} >
-                <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Typography color='textSecondary' variant='body1'>
-                        {title}
-                    </Typography>
-                </Grid>
+                <Grid className={cx(classes.complexHeader)}>
                     <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.EDIT_USER)} checked={permissions.includes(PERMISSIONS.EDIT_USER)} size='small' color='default' />
+                        <Typography className={classes.title_bold}
+                                    variant='body1'>
+                            {title}
+                        </Typography>
                     </Grid>
                     <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.EDIT_SALARY)} checked={permissions.includes(PERMISSIONS.EDIT_SALARY)} size='small' color='default' />
-                    </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.EDIT_DRUG)} checked={permissions.includes(PERMISSIONS.EDIT_DRUG)} size='small' color='default' />
-                    </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.EDIT_HCF)} checked={permissions.includes(PERMISSIONS.EDIT_HCF)} size='small' color='default' />
-                    </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.EDIT_PHARMACY)} checked={permissions.includes(PERMISSIONS.EDIT_PHARMACY)} size='small' color='default' />
-                    </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.EDIT_AGENT)} checked={permissions.includes(PERMISSIONS.EDIT_AGENT)} size='small' color='default' />
+                        <Typography variant='body1'>
+                            <Checkbox
+                                onChange={this.changeHandler(PERMISSIONS.SEE_REPORT)}
+                                checked={permissions.includes(PERMISSIONS.SEE_REPORT)}
+                                size='small' color='default'/>
+                        </Typography>
                     </Grid>
                 </Grid>
 
-                <Grid className={cx(classes.complexHeader)} >
-                <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Typography color='textSecondary' variant='body1'>
-                        {title}
-                    </Typography>
-                </Grid>
+                <Grid className={cx(classes.complexHeader)}>
                     <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.ADD_DRUG)} checked={permissions.includes(PERMISSIONS.ADD_DRUG)} size='small' color='default' />
+                        <Typography color='textSecondary' variant='body1'>
+                            {title}
+                        </Typography>
                     </Grid>
                     <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.ADD_HCF)} checked={permissions.includes(PERMISSIONS.ADD_HCF)} size='small' color='default' />
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.EDIT_USER)}
+                            checked={permissions.includes(PERMISSIONS.EDIT_USER)}
+                            size='small' color='default'/>
                     </Grid>
                     <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.ADD_PHARMACY)} checked={permissions.includes(PERMISSIONS.ADD_PHARMACY)} size='small' color='default' />
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.EDIT_SALARY)}
+                            checked={permissions.includes(PERMISSIONS.EDIT_SALARY)}
+                            size='small' color='default'/>
                     </Grid>
                     <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.ADD_AGENT)} checked={permissions.includes(PERMISSIONS.ADD_AGENT)} size='small' color='default' />
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.EDIT_DRUG)}
+                            checked={permissions.includes(PERMISSIONS.EDIT_DRUG)}
+                            size='small' color='default'/>
+                    </Grid>
+                    <Grid className={cx(classes.cell)} justify='center' container item>
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.EDIT_HCF)}
+                            checked={permissions.includes(PERMISSIONS.EDIT_HCF)}
+                            size='small' color='default'/>
+                    </Grid>
+                    <Grid className={cx(classes.cell)} justify='center' container item>
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.EDIT_PHARMACY)}
+                            checked={permissions.includes(PERMISSIONS.EDIT_PHARMACY)}
+                            size='small' color='default'/>
+                    </Grid>
+                    <Grid className={cx(classes.cell)} justify='center' container item>
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.EDIT_AGENT)}
+                            checked={permissions.includes(PERMISSIONS.EDIT_AGENT)}
+                            size='small' color='default'/>
                     </Grid>
                 </Grid>
 
-                <Grid className={cx(classes.complexHeader)} >
-                <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Typography color='textSecondary' variant='body1'>
-                        {title}
-                    </Typography>
+                <Grid className={cx(classes.complexHeader)}>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary' variant='body1'>
+                            {title}
+                        </Typography>
+                    </Grid>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.ADD_DRUG)}
+                            checked={permissions.includes(PERMISSIONS.ADD_DRUG)}
+                            size='small' color='default'/>
+                    </Grid>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.ADD_HCF)}
+                            checked={permissions.includes(PERMISSIONS.ADD_HCF)}
+                            size='small' color='default'/>
+                    </Grid>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.ADD_PHARMACY)}
+                            checked={permissions.includes(PERMISSIONS.ADD_PHARMACY)}
+                            size='small' color='default'/>
+                    </Grid>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.ADD_AGENT)}
+                            checked={permissions.includes(PERMISSIONS.ADD_AGENT)}
+                            size='small' color='default'/>
+                    </Grid>
                 </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.DELETE_DRUG)} checked={permissions.includes(PERMISSIONS.DELETE_DRUG)} size='small' color='default' />
+
+                <Grid className={cx(classes.complexHeader)}>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Typography color='textSecondary' variant='body1'>
+                            {title}
+                        </Typography>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.DELETE_HCF)} checked={permissions.includes(PERMISSIONS.DELETE_HCF)} size='small' color='default' />
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.DELETE_DRUG)}
+                            checked={permissions.includes(PERMISSIONS.DELETE_DRUG)}
+                            size='small' color='default'/>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.DELETE_PHARMACY)} checked={permissions.includes(PERMISSIONS.DELETE_PHARMACY)} size='small' color='default' />
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.DELETE_HCF)}
+                            checked={permissions.includes(PERMISSIONS.DELETE_HCF)}
+                            size='small' color='default'/>
                     </Grid>
-                    <Grid className={cx(classes.cell)} justify='center' container item>
-                    <Checkbox onChange={this.changeHandler(PERMISSIONS.DELETE_AGENT)} checked={permissions.includes(PERMISSIONS.DELETE_AGENT)} size='small' color='default' />
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.DELETE_PHARMACY)}
+                            checked={permissions.includes(PERMISSIONS.DELETE_PHARMACY)}
+                            size='small' color='default'/>
+                    </Grid>
+                    <Grid className={cx(classes.cell)} justify='center'
+                          container item>
+                        <Checkbox
+                            onChange={this.changeHandler(PERMISSIONS.DELETE_AGENT)}
+                            checked={permissions.includes(PERMISSIONS.DELETE_AGENT)}
+                            size='small' color='default'/>
                     </Grid>
                 </Grid>
             </Grid>
