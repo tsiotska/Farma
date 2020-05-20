1a621d2d8ea308ac0609e6e2a67aad62ca1a994c
Vitalii Tsisaruk <v.tsisaruk@cleverdec.com>
2020-05-20 14:06:53 +0300

    added excel to admin panel

--- app/src/containers/AdminPage/Header/Header.tsx
+++ app/src/containers/AdminPage/Header/Header.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { withStyles, createStyles, WithStyles, Grid, Typography } from '@material-ui/core';
-import { observer } from 'mobx-react';
+import { withStyles, createStyles, WithStyles, Grid, Typography, IconButton } from '@material-ui/core';
+import { inject, observer } from 'mobx-react';
 import SalesModeSwitch from '../../../components/SalesModeSwitch';
 import DateRangeButton from '../../../components/DateRangeButton';
+import ExcelIcon from '../../../components/ExcelIcon';
 
 const styles = (theme: any) => createStyles({
     root: {
@@ -11,16 +12,34 @@ const styles = (theme: any) => createStyles({
     salesModeSwitch: {
         width: 'auto',
         marginBottom: 0
+    },
+    excelIcon: {
+        marginLeft: 'auto'
     }
 });
 
-interface IProps extends WithStyles<typeof styles> {}
-
+interface IProps extends WithStyles<typeof styles> {
+    loadSalesExcel?: () => void;
+}
+@inject(({
+             appState: {
+                 salesStore: {
+                     loadSalesExcel
+                 },
+             }
+         }) => ({
+    loadSalesExcel
+}))
 @observer
 class Header extends Component<IProps> {
+
+    excelClickHandler = () => {
+        const { loadSalesExcel } = this.props;
+        loadSalesExcel();
+    }
+
     render() {
         const { classes } = this.props;
-
         return (
             <Grid className={classes.root} alignItems='center' container>
                 <Typography>
@@ -28,6 +47,9 @@ class Header extends Component<IProps> {
                 </Typography>
                 <DateRangeButton />
                 <SalesModeSwitch classes={{ root: classes.salesModeSwitch }} />
+                <IconButton onClick={this.excelClickHandler}  className={classes.excelIcon} >
+                    <ExcelIcon />
+                </IconButton>
             </Grid>
         );
     }
